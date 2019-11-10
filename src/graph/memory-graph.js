89f7a0470f30e86b31d7024e57b2caccceee4c25
Edward Nicholes Jr <GameDevFox@gmail.com>
2019-11-09 22:33:16 -0600

    Memory Graph to Common

---
+++ src/graph/memory-graph.js
@@ -0,0 +1,71 @@
+import { Pipe } from '@gamedevfox/katana';
+
+export const MemoryGraph = () => {
+  const [hashEdge, setHashEdge] = Pipe();
+
+  let count = 0;
+
+  const edgeMap = {};
+  const headMap = {};
+  const tailMap = {};
+
+  const write = edge => {
+    if(!setHashEdge())
+      throw new Error('`hashEdge` input not set');
+
+    const [head, tail] = edge;
+
+    const id = hashEdge(head, tail);
+    if(edgeMap[id])
+      return id;
+
+    edgeMap[id] = edge;
+
+    const headIdx = headMap[head] || new Set();
+    headIdx.add(tail);
+    headMap[head] = headIdx;
+
+    const tailIdx = tailMap[tail] || new Set();
+    tailIdx.add(head);
+    tailMap[tail] = tailIdx;
+
+    count++;
+
+    return id;
+  };
+
+  const read = id => edgeMap[id];
+
+  const erase = id => {
+    if(!edgeMap[id])
+      return undefined;
+
+    const [head, tail] = edgeMap[id];
+    delete edgeMap[id];
+
+    delete headMap[head][tail];
+    delete tailMap[tail][head];
+
+    count--;
+
+    return [head, tail];
+  };
+
+  const heads = tail => {
+    const set = tailMap[tail];
+    return set ? new Set(set) : new Set();
+  };
+
+  const tails = head => {
+    const set = headMap[head];
+    return set ? new Set(set) : new Set();
+  };
+
+  const countFn = () => count;
+  const list = () => Object.values(edgeMap);
+
+  return {
+    hashEdge: setHashEdge, // Inputs
+    read, write, erase, heads, tails, count: countFn, list, // Outputs
+  };
+};
---
+++ src/graph/memory-graph.spec.js
@@ -0,0 +1,44 @@
+import { expect } from 'chai';
+
+import { RANDOM } from '../index';
+import { hashEdge, pseudoRandom } from '../hash';
+
+import { MemoryGraph } from './memory-graph';
+
+it('MemoryGraph', () => {
+  const random = pseudoRandom(RANDOM);
+  const nodes = [];
+  for(let i = 0; i < 5; i++)
+    nodes.push(random());
+
+  const graph = MemoryGraph();
+  graph.hashEdge(hashEdge);
+
+  for(let i = 0; i < 20; i++) {
+    const head = nodes[i % 5];
+    const tail = nodes[Math.floor(i * 13 / 7) % 5];
+
+    const edge = [head, tail];
+    graph.write(edge);
+  }
+
+  graph.count().should.equal(16);
+
+  const edge = graph.list()[2];
+  edge.should.deep.equal([nodes[2], nodes[3]]);
+
+  const id = hashEdge(edge[0], edge[1]);
+  graph.read(id).should.deep.equal(edge);
+
+  Array.from(graph.heads(nodes[0]))
+    .should.have.members([nodes[0], nodes[3], nodes[1], nodes[4]]);
+  Array.from(graph.heads(nodes[1]))
+    .should.have.members([nodes[1], nodes[4], nodes[2]]);
+
+  const removedEdge = graph.erase(id);
+  removedEdge.should.deep.equal([nodes[2], nodes[3]]);
+
+  graph.count().should.equal(15);
+
+  expect(graph.read(RANDOM)).to.be.undefined;
+});
